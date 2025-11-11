e2c56b8464a97a7aa17d995f30ef53aecc41640f
Fulipe <filipecraveiravaz@gmail.com>
2025-11-11 12:08:57 +0200

    MyEvents Student

--- viko-app/src/app/pages/private/student/my-events-student/my-events-student.component.ts
+++ viko-app/src/app/pages/private/student/my-events-student/my-events-student.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MyeventsComponent } from "../../myevents/myevents.component";
+import { EventFetched } from '../../../../interfaces/interfaces';
+import { EventService } from '../../../../services/event.service';
 
 @Component({
   selector: 'app-my-events-student',
@@ -8,5 +10,45 @@ import { MyeventsComponent } from "../../myevents/myevents.component";
   styleUrl: './my-events-student.component.scss'
 })
 export class MyEventsStudentComponent {
+  private eventsService = inject(EventService)
+
+  upcomingEvents: EventFetched[] = [];
+  finishedEvents: EventFetched[] = [];
+
+  loading = true
+
+  ngOnInit(): void {
+    this.loadEvents()
+  }
+
+  private loadEvents() {
+    this.eventsService.getStudentEvents().subscribe({
+      next: (res) => {
+        // console.log(res)
+        if (res === false) {
+          console.log("Nenhum evento registado.");
+          this.loading = false;
+          return;
+        }
+
+        const e: any = res;
+        const events: EventFetched[] = e
+
+        //Filters finished events
+        this.upcomingEvents = events.filter(event => event.eventStatus != 3);
+
+        //Filters not finished events
+        this.finishedEvents = events
+          .filter(event => event.eventStatus != 1)
+          .filter(event => event.eventStatus != 2);
+
+        this.loading = false;
+      },
+      error: (_) => {
+        console.error("Error while loading registered events: ", _)
+        this.loading = false;
+      }
+    })
+  }
 
 }
