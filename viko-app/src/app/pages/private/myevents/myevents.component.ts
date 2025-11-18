d92692991512aeb17c1f75f9958493e8d4581288
Fulipe <filipecraveiravaz@gmail.com>
2025-11-18 16:50:28 +0200

    Added Delete button to My events page (Teacher)

--- viko-app/src/app/pages/private/myevents/myevents.component.ts
+++ viko-app/src/app/pages/private/myevents/myevents.component.ts
@@ -1,8 +1,8 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
+import { Component, inject, input, Input, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { EventFetched } from '../../../interfaces/interfaces';
 import { EventService } from '../../../services/event.service';
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-myevents',
@@ -15,10 +15,38 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
 export class MyeventsComponent {
   authService = inject(AuthService)
   eventsService = inject(EventService)
+  router = inject(Router)
+  
+  deletePopup: boolean = false;
+  eventGuid!: string
 
   loading = true;
 
   @Input() upcomingEvents: EventFetched[] = [];
   @Input() finishedEvents: EventFetched[] = [];
+  @Input() isTeacher!: boolean;
 
+  onDeleteClick(guid: string){
+    this.deletePopup = true
+    this.eventGuid = guid
+
+  }
+
+  onCancelPopup(){
+    this.deletePopup = false
+  }
+
+  confirmDelete(){
+    this.eventsService.deleteEvent(this.eventGuid).subscribe({
+      next: (res)=>{
+        window.location.reload()
+        // this.router.navigate(['/private/myevents'])
+        this.deletePopup = false
+
+      },
+      error: (_)=>{
+        console.log("Event wasn't deleted! ")
+      }
+    })
+  }
 }
